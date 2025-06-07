5459a1854ff4ae6b0cd0cf190963a165d2a3e4d1
job kilonzo <32642682+jobkilonzo@users.noreply.github.com>
2025-06-07 22:20:46 +0300

    Added login and logout functionality

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import bcrypt from 'bcryptjs'
 import db from '../database/db.js'
+import jwt from 'jsonwebtoken';
+import { SECRET_KEY } from '../config/env.js';
 export const register = async (req, res, next) => {
     try {
         // Check if user exists
@@ -33,9 +35,50 @@ export const register = async (req, res, next) => {
         return res.status(500).json({ message: "Internal server error", error });
     }
 };
-export const login =async (req, res, next) => {
-    
+export const login = async (req, res, next) => {
+    // try {
+
+    // } catch (error) {
+    //     return res.status(500).json({ message: "Internal server error", error });
+    // }
+    // Check if user exists
+
+    const checkQuery = "SELECT * FROM users WHERE username = ?";
+    db.query(checkQuery, [req.body.username], (err, data) => {
+        console.log("DB query complete");
+
+        if (err) {
+            console.error("DB Error:", err);
+            return res.status(500).json(err);
+        }
+
+        if (data.length === 0) {
+            return res.status(409).json("User not found");
+        }
+
+        const user = data[0];
+
+        if (!user.password) {
+            return res.status(500).json("Password not found in database");
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json("Wrong password or username");
+        }
+
+        const token = jwt.sign({ id: user.id }, SECRET_KEY);
+
+        const { password, ...others } = user;
+        res.cookie("accessToken", token, {
+            httpOnly: true
+        }).status(200).json(others);
+    });
 }
-export const logout =async (req, res, next) => {
-    
+export const logout = async (req, res, next) => {
+    res.clearCookie("accessToke", {
+        secure: true,
+        sameSite: "none"
+    }).status(200).json("user has being logged out")
 }
