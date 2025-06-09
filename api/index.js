4e9a92cff43008dc7ab3b51595b104ea1b0257fe
job kilonzo <32642682+jobkilonzo@users.noreply.github.com>
2025-06-09 08:30:07 +0300

    Hit post api and solved backend issues on posts

--- api/index.js
+++ api/index.js
@@ -10,12 +10,19 @@ import cors from "cors";
 
 const app = express();
 
-
+//middlewares
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json());
-app.use(cors());              
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));              
 app.use(cookieParser());      
 
-
+// routes
 app.use("/api/auth", authRouter);
 app.use("/api/comments", commentsRoute);
 app.use("/api/likes", likesRoute);
